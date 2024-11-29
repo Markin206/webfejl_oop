ca7f9ba5c94332723840cac22eba45ebf3a5f732
Lorant Marton <lorant.marton@BOLYAI.LOCAL>
2024-11-29 12:58:19 +0100

    after 2015. usage of class

--- basics/scripts.js
+++ basics/scripts.js
@@ -1,11 +1,49 @@
-const gomszab = new Player("mark");
-function Player(nickname){
-    this.nickname = nickname
-    this.playedMatch = 0;
-}
-/**
- * ----------------------------------------- Saját
-Player.prototype.play = function(){
+
+// function Player(nickname){
+//     this.nickname = nickname
+//     this.playedMatch = 0;
+// }
+// /**
+//  * ----------------------------------------- Saját
+// Player.prototype.play = function(){
+//     this.playedMatch++;
+//     this.endofmatch = "LOST";
+//     const result = Math.floor(Math.random()* 2 - 0)
+//     if(result == 1){
+//         this.endofmatch = "WIN"
+//         console.log(this.nickname, this.playedMatch, this.endofmatch, gomszab.gettierlevel())
+//     }
+//     else{
+//         console.log(this.nickname, this.playedMatch, this.endofmatch, gomszab.gettierlevel())
+//     }
+// }
+// */ ----------------------------------------------------
+
+// Player.prototype.play = function(){
+//     this.playedMatch++;
+//     console.log(this.nickname, this.playedMatch, gomszab.gettierlevel())
+// }
+
+// Player.prototype.gettierlevel = function(){
+//     if(this.playedMatch <= 3){
+//         return "A";
+//     }
+//     else if(this.playedMatch > 3 || this.playedMatch <= 7){
+//         return "B";
+//     }
+//     else{
+//         return "C";
+//     }
+// }
+
+class Player{
+    constructor(nickname){
+        this.nickname = nickname;
+        this.playedMatch = 0;
+    }
+
+
+play(){
     this.playedMatch++;
     this.endofmatch = "LOST";
     const result = Math.floor(Math.random()* 2 - 0)
@@ -17,28 +55,17 @@ Player.prototype.play = function(){
         console.log(this.nickname, this.playedMatch, this.endofmatch, gomszab.gettierlevel())
     }
 }
-*/
 
-Player.prototype.play = function(){
-    this.playedMatch++;
-    console.log(this.nickname, this.playedMatch, gomszab.gettierlevel())
-}
-
-Player.prototype.gettierlevel = function(){
-    if(this.playedMatch <= 3){
+gettierlevel(){
+    if(this.playedMatch <= 3)
         return "A";
-    }
-    else if(this.playedMatch > 3 || this.playedMatch <= 7){
+    else if(this.playedMatch > 3 || this.playedMatch < 8)
         return "B";
-    }
-    else{
+    else
         return "C";
-    }
 }
-
-
-
-
+}
+const gomszab = new Player("mark");
 console.log(gomszab);
 gomszab.play();
 gomszab.gettierlevel();
