6c7b51f05b50eabaa61887f7df217312f8e54feb
Lorant Marton <lorant.marton@BOLYAI.LOCAL>
2024-12-13 11:40:17 +0100

    Person class and render function

--- table/scripts.js
+++ table/scripts.js
@@ -19,3 +19,48 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+
+class Person{
+//mi lesz a célja az osztálynak:
+//1.firstname1, 2, és a lastname elraktározása
+//2.init függvényeb végig iterlálunk az array-en
+//3.objektízáljuk a persont
+//4.person példányokat hozzáfüzzűk a tbodyhoz
+
+    constructor(obj){//objektum lesz a paramétere | komplex constructor
+        this.firstname1 = obj.firstname1,
+        this.firstname2 = obj.firstname2,
+        this.lastname = obj.lastname
+    }
+
+    render(parentelement){//HTMLelement lesz a parent element
+        const tablerow = document.createElement('tr') //sor létrehozása
+        parentelement.appendChild(tablerow)//sor hozzáadása a HTMLelementhez
+
+        const cellfirstname1 = document.createElement('td')//cellák létrehozása
+        cellfirstname1.innerHTML = this.firstname1//cellák tartalma kiegészítjük a Person osztály "firstname1" tulajdonságával
+        tablerow.appendChild(cellfirstname1)//cella hozzáadása a sorhoz
+
+
+        //ugyan az mint a firstname1 cella létre hozzásánál-----------------------------
+        const cellfirstname2 = document.createElement('td')
+        cellfirstname2.innerHTML = this.firstname2
+        if(this.firstname2 ==  undefined){//ha a firstname2 üres/undefined akkor a firstname1 cella colsapnje = 2
+            cellfirstname1.colSpan = 2;
+        }
+        else{//ha meg nem undefined akkor hozzá adja a sorhoz a cellát
+            tablerow.appendChild(cellfirstname2)
+        }
+
+        const celllastname = document.createElement('td')
+        celllastname.innerHTML = this.lastname
+        tablerow.appendChild(celllastname)
+        //------------------------------------------------------------------------------
+    }
+}
+
+function init(){
+    for(const i of array){
+
+    }
+}
