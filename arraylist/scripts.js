cff366e8ab56113356ce19ace726f78ea2f84889
Lorant Marton <lorant.marton@BOLYAI.LOCAL>
2025-01-17 11:39:18 +0100

    ArrayList class letrehozasa

---
+++ arraylist/scripts.js
@@ -0,0 +1,23 @@
+class ArrayList{
+    /**
+     * @type {number}//megadjuk a #lenght tulajdonságnak típusát (szám)
+     */
+    #lenght//megadunk egy privát tulajdonságot (#+megadott tulajdonság neve) //hosszt tartozó tulajdonság
+    #objektum//megadunk egy privát tulajdonságot (#+megadott tulajdonság neve) //belső státusz zárt tart objektum
+    get Count(){//get függvény
+        return this.#lenght;//visszahívjuk a hossz tulajdonságot a this.-al
+    }
+    constructor(){
+        this.#lenght = 0;//hosszt inicializájuk
+        this.#objektum = {};//objektumot inicializájuk
+    }
+    Add(item){
+        const index = this.#lenght;//elkérjük és eltároljuk az aktuális hosszt
+        this.#objektum[index] = item;//beállítjuk a belső státuszunk index tulajdonságnak a bemeneti paramétert
+        this.#lenght++;//a hossz tulajdonság értékét inkrementáljuk/növeljük
+    }
+}
+const csirke = {}
+    csirke.a = 'def';
+    csirke[0] = 'asd';
+console.log(csirke)//kilogoljuk a csirkét
