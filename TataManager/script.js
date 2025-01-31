95b601ebb3a0456e82f2d570308ea2974f26162e
Lorant Marton <lorant.marton@BOLYAI.LOCAL>
2025-01-31 11:31:16 +0100

    DataManager array es filter kesz

--- TataManager/script.js
+++ TataManager/script.js
@@ -1,3 +1,72 @@
-class DataManager {}
+/**
+ * @typedef {{nev:string, eletkor:Number}} Person //típust rendelünk a tulajdonságokhoz
+ * @callback UpdateCallBack
+ * @param {Person[]} persons
+ * @returns {void}
+ */
+class DataManager {
+    /**
+     * @type {Person[]}
+     */
+    #array
+    /**
+     * @type {UpdateCallBack}
+     */
+    #updateCallBack
+    /**
+     * 
+     * @param {Person[]} paramArray 
+     */
+    constructor(paramArray = []){
+        array = paramArray;
+        () => {}
+    }
 
+
+    /**
+    * @param {UpdateCallBack} callback
+    */
+    setUpdateCallback(callback){
+        this.#updateCallBack = callback;
+    }
+
+    /**
+     * @param {Person} item
+     */
+    add(item){
+        this.#array.push(item)
+        this.#updateCallBack(this.#array)
+    }
+
+    /**
+     * @param {string} nev 
+     */
+    filterName(name){
+        const result = [];
+        for(const elem of this.Person)
+        {
+            if(elem.nev === name)
+            {
+            result.push(elem)
+            }
+        }
+        
+        this.#updateCallBack(result)
+    }
+
+    /**
+     * 
+     * @param {Number} eletkor 
+     */
+    filterKor(age){
+        const result = [];
+        for(const elem of this.Person)
+        {
+        if(elem.eletkor === age)
+            {
+            result.push(elem)
+            }
+        }
+    }
+}
 class DataTable {}
